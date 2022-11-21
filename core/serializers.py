8a754c9e6c9101b467d48ab253808f16d522fb30
midhun98 <midhunskani@gmail.com>
2022-11-21 21:57:19 +0530

    serializer changes

--- core/serializers.py
+++ core/serializers.py
@@ -2,12 +2,11 @@ from rest_framework import serializers
 from . import models
 
 class BrandSerializer(serializers.ModelSerializer):
-    # market_place = serializers.CharField(source='market_place.name')
-    # zip_code = serializers.CharField(source='zip_code.code')
+    market_place = serializers.CharField(source='market_place.name', read_only=True)
+    zip_code = serializers.CharField(source='zip_code.code', read_only=True)
     class Meta:
         model = models.Brand
         fields = '__all__'
 
-    # def create(self, validated_data):
-    #     print(validated_data)
-    #     return models.Brand.objects.create(**validated_data)
+    def create(self, validated_data):
+        return models.Brand.objects.create(**validated_data)
