293a862484db86594ef68d9c4bf27bb211077f9c
midhun98 <midhunskani@gmail.com>
2022-11-21 12:50:18 +0530

    front end

--- frontend/views.py
+++ frontend/views.py
@@ -3,5 +3,14 @@ from core import models
 # Create your views here.
 
 def list(request):
-    return  render(request, 'frontend/list.html')
+    return render(request, 'frontend/list.html')
 
+
+def createBrand(request):
+    market = models.MarketPlace.objects.all()
+    zipcode = models.ZipCode.objects.all()
+    context = {
+        "market": market,
+        "zipcode": zipcode
+    }
+    return render(request, 'frontend/create_brand.html', context)
