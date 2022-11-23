bbaf631ae291c0d920b2e21d5260124db0c91437
midhun98 <midhunskani@gmail.com>
2022-11-23 12:17:24 +0530

    front end changes

--- frontend/urls.py
+++ frontend/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('create-brand', views.createBrand, name="create_brand"),
     path("update-brand/<str:pk>", views.updateBrand, name="update-brand"),
     path("content-list", views.contentList, name="content-list"),
+    path("create-content", views.createContent, name="create-content"),
 ]
